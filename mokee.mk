19bc7589b1eae6f788c991e16f02fcbe8fd10b79
martincz <martincz@mokeedev.com>
2014-02-09 23:15:45 +0800

    mokee bringup
    Change-Id: I738fe74fe38aa070ead2a4e086a2e396f813d54f

--- mokee.mk
+++ mokee.mk
@@ -1,8 +1,8 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -10,7 +10,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := MOTO X
-PRODUCT_NAME := cm_ghost
+PRODUCT_NAME := mk_ghost
 
 $(call inherit-product, device/motorola/ghost/full_ghost.mk)
 
