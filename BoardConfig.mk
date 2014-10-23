0a64e63b627e344be42f4b30faf2a5b1aef4dc65
martincz <martincz@mokeedev.com>
2014-10-23 15:49:49 +0800

    Revert "ghost: fix dexpreopt"
    This reverts commit 0ab5c67afbc299ec467ff899434dd00d7a04a424.
    
    Conflicts:
    	BoardConfig.mk
    
    Change-Id: I18db5e458bd078e18b77bf0173e37c4f08a6436a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,6 +202,3 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_MODULE_NAME := "wlan"
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
-
-# Fix DEXPREOPT EXT
-PRODUCT_BOOT_JARS := $(subst $(space),:,$(PRODUCT_BOOT_JARS))
